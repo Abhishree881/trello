8d0bf34aeedad6ed08cb2b3e714b6b1b862c52e4
Abhishree <abhishree881@gmail.com>
2023-12-05 22:54:24 +0530

    home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,5 @@
 // pages/HomePage.js
-import React from "react";
+import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { addBoard } from "../slices/boardSlice";
@@ -8,17 +8,26 @@ const HomePage = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const boards = useSelector((state) => state.board);
+  const [newBoardTitle, setNewBoardTitle] = useState("");
 
   const handleAddBoard = () => {
+    if (newBoardTitle.trim() === "") {
+      // Don't add a board with an empty title
+      return;
+    }
+
     dispatch(
       addBoard({
         newBoard: {
           id: Date.now(),
-          title: `New Board ${Date.now()}`,
+          title: newBoardTitle,
           lists: [],
         },
       })
     );
+
+    // Clear input after adding a new board
+    setNewBoardTitle("");
   };
 
   const handleBoardClick = (boardId) => {
@@ -26,18 +35,69 @@ const HomePage = () => {
   };
 
   return (
-    <div>
-      <h1>Welcome to the Home Page!</h1>
-      <button onClick={handleAddBoard}>Add Board</button>
-      <div>
+    <div style={styles.container}>
+      <h1 style={styles.heading}>Welcome to the Home Page!</h1>
+      <div style={styles.addBoardContainer}>
+        <input
+          type="text"
+          value={newBoardTitle}
+          onChange={(e) => setNewBoardTitle(e.target.value)}
+          placeholder="Enter new board title"
+          style={styles.input}
+        />
+        <button onClick={handleAddBoard} style={styles.addButton}>
+          Add Board
+        </button>
+      </div>
+      <div style={styles.boardButtonsContainer}>
         {boards.map((board) => (
-          <button key={board.id} onClick={() => handleBoardClick(board.id)}>
+          <button
+            key={board.id}
+            onClick={() => handleBoardClick(board.id)}
+            style={styles.boardButton}
+          >
             {board.title}
           </button>
         ))}
       </div>
     </div>
   );
+};
+
+const styles = {
+  container: {
+    textAlign: "center",
+    padding: "20px",
+  },
+  heading: {
+    fontSize: "24px",
+    marginBottom: "20px",
+  },
+  addBoardContainer: {
+    marginBottom: "20px",
+  },
+  input: {
+    padding: "8px",
+    marginRight: "8px",
+  },
+  addButton: {
+    padding: "8px",
+    cursor: "pointer",
+  },
+  boardButtonsContainer: {
+    display: "flex",
+    justifyContent: "center",
+    flexWrap: "wrap",
+  },
+  boardButton: {
+    padding: "10px",
+    margin: "8px",
+    cursor: "pointer",
+    backgroundColor: "#3498db",
+    color: "#fff",
+    border: "none",
+    borderRadius: "5px",
+  },
 };
 
 export default HomePage;
